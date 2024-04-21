3e9344719529c065250d8dc5235ddae3da66c8a5
Opeyemi1122 <alabiopex@gmail.com>
2024-04-21 13:50:35 +0200

    ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
+"""Module that lists all states from mySQL database"""
+import sys
+import MySQLdb
 
-if __name__ == '__main__':
-    from sys import argv
-    import MySQLdb as mysql
-
-    try:
-        db = mysql.connect(host='localhost', port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3])
-    except Exception:
-        print('Failed to connect to the database')
-        exit(0)
-
+def list_states (username, password, database):
+    """lists all states from the database hbtn_0e_0_usa.
+    Ags:
+        username: mysql username
+        password: mysql password
+        database: mysql database
+    """
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host='localhost',\
+            port=3306,\
+            user=username,\
+            passwd=password,\
+            db=database)
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    # Execute the SQL query to fetch all states
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    result_query = cursor.fetchall()
+    # Fetch all the rows from the query result
+    rows = cursor.fetchall()
 
-    for row in result_query:
+    # Print the results
+    for row in rows:
         print(row)
 
-    cursor.close()
+    # Close the database connection
     db.close()
+
+# Example usage
+if __name__ == '__main__':
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
