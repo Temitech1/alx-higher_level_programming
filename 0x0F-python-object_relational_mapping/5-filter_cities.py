8b747e6e81dc2a5b9333e8acf7ebf521332e8c80
Opeyemi1122 <alabiopex@gmail.com>
2024-04-26 15:23:29 +0200

    filter cities updated

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-"""Lists all cities from the database hbtn_0e_4_usa"""
+"""
+Takes in the name of a state as an argument and
+lists all cities of that state, using
+the database hbtn_0e_4_usa
+"""
 
 if __name__ == '__main__':
     from sys import argv
     import MySQLdb as mysql
+    import re
 
-    if (len(argv) != 4):
-        print('Use: username, password, database name')
+    if (len(argv) != 5):
+        print('Use: username, password, database name, state name')
+        exit(1)
+
+    state_name = ' '.join(argv[4].split())
+
+    if (re.search('^[a-zA-Z ]+$', state_name) is None):
+        print('Enter a valid name state (example: California)')
         exit(1)
 
     try:
@@ -18,15 +29,20 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
 
-    cursor.execute("""SELECT c.id, c.name, s.name FROM cities as c
+    cuantity = cursor.execute("""SELECT c.name FROM cities as c
                       INNER JOIN states as s
                       ON c.state_id = s.id
-                      ORDER BY c.id ASC;""")
+                      WHERE s.name = '{:s}'
+                      ORDER BY c.id ASC;""".format(state_name))
 
     result_query = cursor.fetchall()
 
-    for row in result_query:
-        print(row)
+    final = []
+
+    for i in range(cuantity):
+        final.append(result_query[i][0])
+
+    print(', '.join(final))
 
     cursor.close()
     db.close()
