29583d1e10823b027c8a3dea00ac23bf4b16e83c
Opeyemi1122 <alabiopex@gmail.com>
2024-04-26 15:29:31 +0200

    model state fetch all

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a City
+Contains State class and Base, an instance of declarative_base()
 """
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+mymeta = MetaData()
+Base = declarative_base(metadata=mymeta)
 
 
-class City(Base):
+class State(Base):
     """
-    Representation of the table cities
+    Class with id and name attributes of each state
     """
-    __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
